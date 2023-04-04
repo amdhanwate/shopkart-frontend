9e800451c1848e6743063691e58c4a8a28faa9c8
Abhijeet Dhanwate <62416129+amdhanwate@users.noreply.github.com>
2023-04-04 14:05:28 +0530

    use backend hosted on render

--- src/app/global.variables.ts
+++ src/app/global.variables.ts
@@ -2,6 +2,7 @@
 
 "use strict";
 
-export const hostedAPI = "https://backend-shopkart.herokuapp.com"
+// export const hostedAPI = "https://backend-shopkart.herokuapp.com"
+export const hostedAPI = "https://shopkart-backend-9lhx.onrender.com"
 export const localAPI = "http://localhost:3300/"
 
